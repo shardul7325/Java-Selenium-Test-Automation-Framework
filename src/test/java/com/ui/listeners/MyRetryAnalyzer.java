d1c24f2c6395509d42e6295f3aac99db47c9b3d6
shard <shard@192.168.10.55>
2025-04-01 12:57:09 +0530

    Committed the remaining file for retryAnalyzer.

---
+++ src/test/java/com/ui/listeners/MyRetryAnalyzer.java
@@ -0,0 +1,27 @@
+package com.ui.listeners;
+
+import org.testng.IRetryAnalyzer;
+import org.testng.ITestResult;
+
+import com.constants.EnvironmentConstants;
+import com.utility.JSONUtility;
+
+import static com.utility.PropertiesUtil.*;
+
+public class MyRetryAnalyzer implements IRetryAnalyzer{
+
+//	private static final int MAX_NUMBER_OF_ATTEMPTS = Integer.parseInt(readProperty(EnvironmentConstants.QA, "MAX_NUMBER_OF_ATTEMPTS"));
+	private static final int MAX_NUMBER_OF_ATTEMPTS = JSONUtility.readJSON(EnvironmentConstants.QA).getmaxNumberOfAttempts();
+	private static int currentAttempt = 1;
+	
+	@Override
+	public boolean retry(ITestResult result) {
+		if(currentAttempt <= MAX_NUMBER_OF_ATTEMPTS) {
+			currentAttempt++;
+			return true;
+		}
+		
+		return false;
+	}
+
+}
